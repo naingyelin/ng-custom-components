0dd3195309b00cad54b2a920145992d45f2febf2
naing ye lin thein <nyl.thein@gmail.com>
2019-07-02 07:38:26 +1000

    custom input controls done

--- my-app/src/app/my-currency-component-module/my-currency-component/my-currency.component.ts
+++ my-app/src/app/my-currency-component-module/my-currency-component/my-currency.component.ts
@@ -1,55 +1,12 @@
-import { Component, OnInit, OnDestroy, Self, Optional, ViewChild, ElementRef, Input, AfterViewInit, HostBinding } from '@angular/core';
+import { Component, OnInit, OnDestroy, Self, Optional, ViewChild, ElementRef, Input, AfterViewInit } from '@angular/core';
 import { AbstractControl, NG_VALUE_ACCESSOR, Validators, NgControl, ControlValueAccessor, ValidatorFn } from '@angular/forms';
-import { MatFormFieldControl } from '@angular/material';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-my-currency',
   templateUrl: './my-currency-component.component.html',
-  styleUrls: ['./my-currency-component.component.sass'],
-  providers: [{ provide: MatFormFieldControl, useExisting: MyCurrencyComponent }]
+  styleUrls: ['./my-currency-component.component.sass']
 })
-export class MyCurrencyComponent implements OnInit, OnDestroy, AfterViewInit, ControlValueAccessor, MatFormFieldControl<any> {
-  static nextId = 0;
-  // tslint:disable-next-line: variable-name
-  private _placeholder: string;
-  // tslint:disable-next-line: variable-name
-  private _required = false;
-  // tslint:disable-next-line: variable-name
-  private _value = '';
-  @HostBinding('attr.aria-describedby') describedBy = '';
-  errorState = false;
-  controlType = `app-my-currency`;
-  autofilled?: boolean;
-  @Input() get value(): string | null {
-    return this._value;
-  }
-  set value(val: string) {
-    this._value = val;
-    this.stateChanges.next();
-  }
-  stateChanges = new Subject<void>();
-  @HostBinding() id = `app-my-number-${MyCurrencyComponent.nextId++}`;
-  ngControl: NgControl;
-  focused = false;
-  get empty(): boolean {
-    return (this._value && false) || this._value === '';
-  }
-  shouldLabelFloat: boolean;
-  @Input()
-  get required(): boolean {
-    return this._required;
-  }
-  set(req: boolean) {
-    this._required = req;
-    this.stateChanges.next();
-  }
-  setDescribedByIds(ids: string[]): void {
-    this.describedBy = ids.join[' '];
-  }
-  onContainerClick(event: MouseEvent): void {
-    throw new Error("Method not implemented.");
-  }
+export class MyCurrencyComponent implements OnInit, OnDestroy, AfterViewInit, ControlValueAccessor {
   control: AbstractControl;
   @Input() validators: ValidatorFn[];
   @ViewChild('input', null) input: ElementRef<any>;
@@ -57,15 +14,7 @@ export class MyCurrencyComponent implements OnInit, OnDestroy, AfterViewInit, Co
   onChange: () => void;
   @Input() disabled: boolean;
   @Input() label: string;
-  @Input()
-  get placeholder() {
-    return this._placeholder;
-  }
-  set placeholder(plh) {
-    this._placeholder = plh;
-    this.stateChanges.next();
-  }
-
+  @Input() placeholder = '';
   constructor(@Self() @Optional() private controlDirective: NgControl) {
     this.controlDirective.valueAccessor = this;
   }
